0d9d43b897741bb53ca56f297c6e8c14dee4ca38
JasonOyugi <Jason.oyugi@gatsbyafrica.org.uk>
2026-08-25 07:40:11 +0300

    Initial commit

--- prodigy-glucare/src/app/shop/page.tsx
+++
@@ -1,5 +0,0 @@
-import { Navigate } from "react-router-dom"
-
-export default function ShopIndexPage() {
-  return <Navigate to="/shop/wellness-products" replace />
-}
--- prodigy/src/app/shop/page.tsx
+++
@@ -1,7 +0,0 @@
-"use client"
-
-import { Navigate } from "react-router-dom"
-
-export default function ShopIndexPage() {
-  return <Navigate to="/shop/wellness-products" replace />
-}
